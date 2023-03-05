7726389d98b8ccfdfc90fb1465877090340e681c
atiikmahmud <atikmahmud.cse@gmail.com>
2023-03-05 18:37:54 +0600

    Bootstrap linking in Function

--- functions.php
+++ functions.php
@@ -5,3 +5,22 @@
 
 // Theme Title
 add_theme_support('title-tag');
+
+// Theme CSS and jQuery File Calling
+function atik_css_js_file_calling()
+{
+    // Style CSS 
+    wp_enqueue_style( 'atik-style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.2.3', 'all');
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('custom');
+
+    // Style JS
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.2.3', 'true');
+    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '5.2.3', 'true');
+}
+add_action('wp_enqueue_scripts', 'atik_css_js_file_calling');
+
+?>
