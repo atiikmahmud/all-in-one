40dd90a1423a642ac87e09ea79c8d767587d2caa
atiikmahmud <atikmahmud.cse@gmail.com>
2023-03-06 13:49:10 +0600

    Setup menu position

--- functions.php
+++ functions.php
@@ -32,21 +32,45 @@ add_action('wp_enqueue_scripts', 'ali_add_google_fonts');
 # Theme Function
 function atik_customizar_register($wp_customize)
 {
+    # Header Area Function
     $wp_customize->add_section('atik_header_area', array(
-        'title' => __('Header Area', 'all-in-one'),
-        'description' => 'If you interested to update your header area, you can do it here.'
+        'title'         => __('Header Area', 'all-in-one'),
+        'description'   => 'If you interested to update your header area, you can do it here.'
     ));
 
     $wp_customize->add_setting('atik_logo', array(
-        'default' => get_bloginfo('template_directory') . '/img/logo.png',
+        'default'       => get_bloginfo('template_directory') . '/img/logo.png',
     ));
 
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'atik_logo', array(
-        'label' => 'Logo Upload',
-        'description' => 'If you interested to change or update your logo, you can do it.',
-        'setting' => 'atik_logo',
-        'section' => 'atik_header_area',
+        'label'         => 'Logo Upload',
+        'description'   => 'If you interested to change or update your logo, you can do it.',
+        'setting'       => 'atik_logo',
+        'section'       => 'atik_header_area',
     ) ));
+
+    # Menu Position Option
+    $wp_customize->add_section('atik_menu_option', array(
+       'title'          => __('Menu Position Option', 'all-in-one'),
+       'description'    => 'If you interested to change your menu position you can do it .'
+    ));
+
+    $wp_customize->add_setting('atik_menu_position', array(
+        'default'       => 'right_menu',
+    ));
+
+    $wp_customize->add_control('atik_menu_position', array(
+        'label'         => 'Menu Position',
+        'description'   => 'Select your menu position',
+        'setting'       => 'atik_menu_position',
+        'section'       => 'atik_menu_option',
+        'type'          => 'radio',
+        'choices'       => array(
+            'left_menu'     => 'Left Menu',
+            'right_menu'    => 'Right Menu',
+            'center_menu'   => 'Center Menu'
+        ),
+    ));
 }
 add_action('customize_register', 'atik_customizar_register');
 
