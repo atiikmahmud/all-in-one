2a183a34a1e69e4164c0e4c7b24649004ec95a84
atiikmahmud <atikmahmud.cse@gmail.com>
2023-03-12 16:35:49 +0600

    POst Page, Single Page Setup

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php 
 
-# The main tempalte file
+# The template for displaying pages
 get_header(); 
 
 ?>
@@ -9,7 +9,7 @@ get_header();
     <div class="container">
         <div class="row">
             <div class="col-md-9">
-                <?php get_template_part('template/blog_setup'); ?>
+                <?php get_template_part('template/post_setup'); ?>
             </div>
             <div class="col-md-3">
                 <?php get_sidebar(); ?>
