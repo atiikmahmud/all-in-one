8ebda87b16de3798a307e7201d99700cae242eaf
atiikmahmud <atikmahmud.cse@gmail.com>
2023-03-13 13:15:54 +0600

    404 page added

--- 404.php
+++ 404.php
@@ -1,6 +1,24 @@
-<?php get_header(); ?>
+<?php 
 
-<h1 class="text-center">404</h1>
-<p class="text-center">Page not found!</p>
+# The tempalte for displaying 404 page (Page not found)
+
+get_header(); 
+
+?>
+
+<section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 error_page">
+            <p class="h2 mb-3">404 Error - Page not Found</p>
+            <h1>Oops! Looks Like something was wrong</h1>
+            <div class="error_search">
+                <?php get_search_form(); ?>
+            </div>
+            <a href="<?php echo home_url(); ?>" class="homepage">Homepage</a>
+        </div>
+      </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -17,7 +17,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
-                    <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('atik_logo'); ?>" alt="Logo"></a>
+                    <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('atik_logo'); ?>" alt="Logo" width="80%"></a>
                 </div>
                 <div class="col-md-9">
                     <?php wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'nav')); ?>
