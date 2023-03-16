a648abd556aa6685c8dcd3f290ede1d2470de2e5
atiikmahmud <atikmahmud.cse@gmail.com>
2023-03-16 12:58:33 +0600

    Custom Post with Category Post Show

--- inc/custom_post.php
+++ inc/custom_post.php
@@ -24,6 +24,7 @@ function custom_service()
         'hierarchial' => true,
         'show_ui' => true,
         'capability' => 'post',
+        'taxonomies' => array('category'),
         'rewrite' => array('slug' => 'service'),
         'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
     )); 
@@ -59,3 +60,21 @@ function slider()
     )); 
 }
 add_action('init', 'slider');
+
+# Custom post show with catagories
+function query_post_type($query)
+{
+    if(is_category())
+    {
+        $post_type = get_query_var('post_type');
+        if($post_type)
+        {
+            $post_type = $post_type;
+        } else {
+            $post_type = array('post', 'service');
+            $query->set('post_type', $post_type);
+            return $query;
+        }
+    }
+}
+add_filter('pre_get_posts', 'query_post_type');
